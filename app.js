fe3a55b95cf1bd8174c2f27454e3367af3fc11e0
Jéssica Martins <jesscodes@proton.me>
2026-01-27 19:57:33 -0300

    Tweaked stylesheet, cleaned up and formatted code

--- app.js
+++ app.js
@@ -5,20 +5,36 @@ const resultList = document.getElementById("birds");
 let apiRequest = new XMLHttpRequest();
 
 birdForm.addEventListener("submit", ($event) => {
-    $event.preventDefault();
-    const searchedBird = birdInput.value;
-    apiRequest.open('GET', 'https://xeno-canto.org/api/3/recordings?query=en:' + searchedBird + '&per_page=50&page=1&key=10086c11aefae3c85c6673f71fd634f143b0468d');
-    apiRequest.send();
+  $event.preventDefault();
+  const searchedBird = birdInput.value;
+  apiRequest.open('GET', 'https://xeno-canto.org/api/3/recordings?query=en:' + searchedBird + '&per_page=50&page=1&key=10086c11aefae3c85c6673f71fd634f143b0468d');
+  apiRequest.send();
 });
-
 
 apiRequest.onreadystatechange = () => {
     if (apiRequest.readyState === 4) {
       const response = JSON.parse(apiRequest.response);
       resultList.innerHTML =
       `
-      <div class="card">
-      <iframe src='https:${response.recordings[0].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+      <div class="result-cards">
+        <div class="card">
+        <iframe src='https:${response.recordings[0].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
+        <div class="card">
+        <iframe src='https:${response.recordings[1].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
+        <div class="card">
+        <iframe src='https:${response.recordings[2].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
+        <div class="card">
+        <iframe src='https:${response.recordings[3].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
+        <div class="card">
+        <iframe src='https:${response.recordings[4].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
+        <div class="card">
+        <iframe src='https:${response.recordings[5].url}/embed?simple=1' scrolling='no' frameborder='0' width='500' height='200'></iframe>
+        </div>
       </div>
       `
     } else {
