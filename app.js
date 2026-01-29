b361082635152161790b12d9639edc0019af7d91
Jéssica Martins <jesscodes@proton.me>
2026-01-29 19:40:36 -0300

    Worked on error handling, cleaned up code, added footer to the page.

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const resultList = document.getElementById("birds");
 
 let apiRequest = new XMLHttpRequest();
 
-birdForm.addEventListener("submit", ($event) => {
-  $event.preventDefault();
-  const searchedBird = birdInput.value;
-  apiRequest.open('GET', 'https://xeno-canto.org/api/3/recordings?query=en:' + searchedBird + '&per_page=50&page=1&key=10086c11aefae3c85c6673f71fd634f143b0468d');
-  apiRequest.send();
-});
+function getData() {
+      birdForm.addEventListener("submit", ($event) => {
+      $event.preventDefault();
+      resultList.innerHTML = "Loading..."
+      const searchedBird = birdInput.value;
+      apiRequest.open('GET', 'https://xeno-canto.org/api/3/recordings?query=en:' + searchedBird + '&per_page=50&page=1&key=10086c11aefae3c85c6673f71fd634f143b0468d');
+      apiRequest.send()});
+    };
 
-apiRequest.onreadystatechange = () => {
+function renderData () {
+  apiRequest.onreadystatechange = () => {
     if (apiRequest.readyState === 4) {
       const response = JSON.parse(apiRequest.response);
       resultList.innerHTML =
@@ -37,9 +40,8 @@ apiRequest.onreadystatechange = () => {
         </div>
       </div>
       `
-    } else {
-      resultList.innerHTML =
-      `
-      <p>Loading...</p>
-      `
-}};
+    }
+  }};
+
+getData();
+renderData();
